b138f97678657b66d47bd4555bad0b68552309ea
刘菁菁 <jingjing.lqq@alibaba-inc.com>
2016-01-13 18:30:28 +0800

    修改个人主页的首页的语言切换

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var url = require("url");
+var qs = require("querystring");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var queryurl = url.parse(req.url).query;
+	var ps = qs.parse(queryurl);
+	if(queryurl && ps['lang'] == 'en'){
+		res.render('index_en', { title: 'Homepage of Yadan'});
+	}else{
+		res.render('index', { title: '鸭蛋的主页'});
+	}
 });
 
 module.exports = router;
